9f1bae0b7755cf78a5f3706062b37d883511af8e
nicolasfanin <faninnicolas@gmail.com>
2022-06-04 10:13:56 -0300

    etrofit implementation

--- app/src/main/java/com/tecno_moviles/museum/base/ServiceModule.kt
+++ app/src/main/java/com/tecno_moviles/museum/base/ServiceModule.kt
@@ -3,18 +3,23 @@ package com.tecno_moviles.museum.base
 import com.tecno_moviles.museum.BuildConfig
 import okhttp3.Interceptor
 import okhttp3.OkHttpClient
+import okhttp3.Response
 import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
 
 const val RETROFIT_API_AUTH = "RetrofitAuthApi"
 const val HTTP_CLIENT_AUTH = "OkHttpClientAuth"
 
+const val TIMEOUT_API = 30L
+
 val apiAuthModule = module {
     //Auth Api
-    single(named(HTTP_CLIENT_AUTH)) { providerHttpClientAuth(get(), get()) }
+    single() { providerHttpLoggingInterceptor() }
+    single(named(HTTP_CLIENT_AUTH)) { providerHttpClientAuth(get()) }
     single(named(RETROFIT_API_AUTH)) {
         providerRetrofit(
             url = BuildConfig.BASE_URL,
@@ -31,8 +36,6 @@ fun providerRetrofit(url: String, client: OkHttpClient): Retrofit {
         .build()
 }
 
-
-/*
 fun providerHttpClientAuth(
     httpLoggingInterceptor: HttpLoggingInterceptor?
 ): OkHttpClient {
@@ -51,11 +54,34 @@ fun providerHttpClientAuth(
 
     httpClientBuilder.addInterceptor(ApiKotlinVersionInterceptor())
     return httpClientBuilder.build()
-} */
+}
 
 fun providerHttpLoggingInterceptor(): HttpLoggingInterceptor {
     val logging = HttpLoggingInterceptor()
     logging.level =
         if (BuildConfig.DEBUG) HttpLoggingInterceptor.Level.BODY else HttpLoggingInterceptor.Level.NONE
     return logging
+}
+
+fun generateCustomClient(timeout: Long): OkHttpClient.Builder {
+    return OkHttpClient.Builder()
+        .addInterceptor(BodyInterceptor())
+        .connectTimeout(timeout, TimeUnit.SECONDS)
+        .writeTimeout(timeout, TimeUnit.SECONDS)
+        .readTimeout(timeout, TimeUnit.SECONDS)
+}
+
+class BodyInterceptor : Interceptor {
+    override fun intercept(chain: Interceptor.Chain): Response {
+        val response = chain.proceed(chain.request())
+        return if (response.code == 204 || response.code == 205) {
+            response
+                .newBuilder()
+                .code(200)
+                .body(response.body)
+                .build()
+        } else {
+            response
+        }
+    }
 }
